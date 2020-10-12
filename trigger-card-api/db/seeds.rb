a89478361b173aa3ecf52c6c61354395a0799c28
barrettanderson <barrettanderson1989@gmail.com>
2020-10-12 09:19:40 -0700

    added seed data for triggers

--- trigger-card-api/db/seeds.rb
+++ trigger-card-api/db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+bullying = {
+    name: "Bullying",
+    description: "People using their power to lessen others"
+}
+letting_people_down = {
+    name: "When I let someone down",
+    description: "Disappointing those I care about, or not following through/being able to follow through"
+}
+Trigger.create([
+    bullying,
+    letting_people_down
+])
+
+WarningSign.create([
+    { }
+])
+
+CopingSkill.create([
+    { }
+])
+
+t.string :name
+t.string :description
