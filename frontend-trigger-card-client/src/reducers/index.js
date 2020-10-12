22738b3dd69c24518393afc8060c9c537c71a99f
barrettanderson <barrettanderson1989@gmail.com>
2020-10-12 09:57:45 -0700

    added combineReducers and triggersReducer

---
+++ frontend-trigger-card-client/src/reducers/index.js
@@ -0,0 +1,6 @@
+import triggersReducer from './triggersReducers';
+import{ combineReducers } from 'redux';
+
+export default combineReducers({
+    triggersReducer
+})
---
+++ frontend-trigger-card-client/src/reducers/triggersReducers.js
@@ -0,0 +1,11 @@
+const initialState = {
+    loading: true,
+    triggers: []
+}
+
+export default (state=initialState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
