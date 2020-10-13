6d9d3f4594158b80b456ce4ec54589afad24d971
barrettanderson <barrettanderson1989@gmail.com>
2020-10-13 15:53:52 -0700

    built out list, passed props, mapped state to prop

--- frontend-trigger-card-client/src/containers/WarningSignList.js
+++ frontend-trigger-card-client/src/containers/WarningSignList.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
+import WarningSignCard from '../components/WarningSignCard'
+import { connect } from 'react-redux'
 
-export class WarningSignList extends Component {
+
+class WarningSignList extends Component {
     render() {
+        console.log(this.props)
+        const warning_signs = this.props.warning_signs.map((warning_sign, i) => <WarningSignCard key={i} warning_sign={warning_sign} />)
         return (
             <div>
-                This is a warning sign
+                <h2>Warning Signs</h2>
+                <ul>
+                    { warning_signs }
+                </ul>
             </div>
         )
     }
 }
 
-export default WarningSignList
+const mapStateToProps = state => {
+    return {
+        warning_signs: state.warningSignsReducer
+    }
+}
+
+export default connect(mapStateToProps)(WarningSignList)
