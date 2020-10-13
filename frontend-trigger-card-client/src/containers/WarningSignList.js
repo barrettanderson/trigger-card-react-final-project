bae478cd08a7ccf56d63a743d074ae754b12943a
barrettanderson <barrettanderson1989@gmail.com>
2020-10-13 09:28:26 -0700

    added base rce for warning signs

---
+++ frontend-trigger-card-client/src/containers/WarningSignList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class WarningSignList extends Component {
+    render() {
+        return (
+            <div>
+                This is a warning sign
+            </div>
+        )
+    }
+}
+
+export default WarningSignList
