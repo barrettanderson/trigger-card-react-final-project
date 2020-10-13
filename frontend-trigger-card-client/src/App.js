a21e2fd734b946a823de0b28da207f2e0396a510
barrettanderson <barrettanderson1989@gmail.com>
2020-10-12 18:41:53 -0700

    added warning signs and coping skill lists and routes

--- frontend-trigger-card-client/src/App.js
+++ frontend-trigger-card-client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import NavBar from './components/NavBar'
 import Home from './containers/Home'
+import TriggerList from './containers/TriggerList'
+import WarningSignList from './containers/WarningSignList'
+import CopingSkillList from './containers/CopingSkillList'
 import { connect } from 'react-redux'
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
@@ -23,6 +26,9 @@ class App extends Component {
           <NavBar>
           <Switch>
             <Route exact path="/" component={ Home }/>
+            <Route exact path="/triggers" component={ TriggerList }/>
+            <Route exact path="/warning_signs" component={ WarningSignList }/>
+            <Route exact path="/coping_skills" component={ CopingSkillList }/>
           </Switch>
           </NavBar>
         </div>
