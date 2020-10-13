4e9c13beedb9a097a00face30ad78923bf67c78e
barrettanderson <barrettanderson1989@gmail.com>
2020-10-13 09:28:16 -0700

    added base rce for coping skills

---
+++ frontend-trigger-card-client/src/containers/CopingSkillList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class CopingSkillList extends Component {
+    render() {
+        return (
+            <div>
+                This is a coping skill
+            </div>
+        )
+    }
+}
+
+export default CopingSkillList
