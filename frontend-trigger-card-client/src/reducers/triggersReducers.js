4094ed53e83b22e5164d8ad13ee7713f7567011e
barrettanderson <barrettanderson1989@gmail.com>
2020-10-13 09:27:05 -0700

    fixed SHOW_TRIGGERS

--- frontend-trigger-card-client/src/reducers/triggersReducers.js
+++ frontend-trigger-card-client/src/reducers/triggersReducers.js
@@ -1,7 +1,7 @@
 export default (state=[], action) => {
     switch(action.type) {
         case "SHOW_TRIGGERS":
-            return action.triggers 
+            return action.triggers //triggers: []
         // case "ADD_TRIGGER":
         //     return[...state, action.trigger]     
         default:
