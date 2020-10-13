5c0bb6e0d0d77fb45c308efd69dfbde3bf1dc6eb
barrettanderson <barrettanderson1989@gmail.com>
2020-10-13 16:05:22 -0700

    built out list to map state to props

--- frontend-trigger-card-client/src/containers/CopingSkillList.js
+++ frontend-trigger-card-client/src/containers/CopingSkillList.js
@@ -1,13 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import CopingSkillCard from '../components/CopingSkillCard';
+import { connect } from 'react-redux';
 
 export class CopingSkillList extends Component {
     render() {
+        const coping_skills = this.props.coping_skills.map((coping_skill, i) => <CopingSkillCard key={i} coping_skill={coping_skill}/>)
         return (
             <div>
-                This is a coping skill
+                <h2>Coping Skills</h2>
+                <ul>
+                    { coping_skills }
+                </ul>
             </div>
         )
     }
 }
 
-export default CopingSkillList
+const mapStateToProps = state => {
+    return {
+        coping_skills: state.copingSkillsReducer
+    }
+} 
+
+export default connect(mapStateToProps)(CopingSkillList)
