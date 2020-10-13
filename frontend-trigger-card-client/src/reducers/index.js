30d2e4b578a8592678227b8b39d26265f5d86e38
barrettanderson <barrettanderson1989@gmail.com>
2020-10-13 09:27:39 -0700

    added warning signs and coping skills to combineReducers

--- frontend-trigger-card-client/src/reducers/index.js
+++ frontend-trigger-card-client/src/reducers/index.js
@@ -1,11 +1,11 @@
 import{ combineReducers } from 'redux';
 import triggersReducer from './triggersReducers';
-// import warningSignsReducer from './warningSignsReducers'
-// import copingSkillsReducer from './copingSkillsReducers'
+import warningSignsReducer from './warningSignsReducers'
+import copingSkillsReducer from './copingSkillsReducers'
 
 
 export default combineReducers({
-    triggersReducer
-    // warningSignsReducer, 
-    // copingSkillsReducer
+    triggersReducer,
+    warningSignsReducer, 
+    copingSkillsReducer
 })
