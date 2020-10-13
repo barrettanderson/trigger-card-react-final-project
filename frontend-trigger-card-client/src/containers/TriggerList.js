5d4eacb83dc29738bc483130d41fee45b559318e
barrettanderson <barrettanderson1989@gmail.com>
2020-10-13 15:03:18 -0700

    passed props to TriggerCard component

--- frontend-trigger-card-client/src/containers/TriggerList.js
+++ frontend-trigger-card-client/src/containers/TriggerList.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
+import TriggerCard from '../components/TriggerCard'
 import { connect } from 'react-redux'
 
 class TriggerList extends Component {
-    // const triggers = this.props.triggers.map((trigger, i) => <TriggerCard key={i} trigger={trigger})
     render() {
+        const triggers = this.props.triggers.map((trigger, i) => <TriggerCard key={i} trigger={trigger} />)
         console.log(this.props)
         return (
             <div>
                 <h2>Triggers</h2>
+                <ul>
+                    { triggers }
+                </ul>
 
             </div>
         )
