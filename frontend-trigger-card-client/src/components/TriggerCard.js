7196737b198d1e9cb918b109df626826078d297a
barrettanderson <barrettanderson1989@gmail.com>
2020-10-14 11:36:35 -0700

    imported trigger button

--- frontend-trigger-card-client/src/components/TriggerCard.js
+++ frontend-trigger-card-client/src/components/TriggerCard.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import TriggerButtons from './TriggerButtons'
 
 const TriggerCard = ({ trigger }) => {
+    // console.log(trigger)
     return (
-        <li>{trigger.name} - {trigger.description}</li>
+        <li>{trigger.name} - {trigger.description}  <TriggerButtons trigger={trigger}/></li>
     )
 }
 
