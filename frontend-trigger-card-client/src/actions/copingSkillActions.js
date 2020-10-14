99db2c409d35a069d04e9cb833ce6fe89b55d874
barrettanderson <barrettanderson1989@gmail.com>
2020-10-13 22:07:42 -0700

    added action to create new coping skill

--- frontend-trigger-card-client/src/actions/copingSkillActions.js
+++ frontend-trigger-card-client/src/actions/copingSkillActions.js
@@ -5,10 +5,34 @@ const showCopingSkills = (coping_skills) => {
     }
 }
 
+const addCopingSkill = (coping_skill) => {
+    return {
+        type: "ADD_COPING_SKILL",
+        coping_skill
+    }
+}
+
 export const fetchCopingSkills = () => {
     return (dispatch) => {
         fetch('http://localhost:3001/coping_skills')
             .then( resp => resp.json() )
             .then( coping_skills => dispatch(showCopingSkills(coping_skills)))
+    }
+}
+
+export const createCopingSkill = (copingSkillData) => {
+    return (dispatch) => {
+        fetch('http://localhost:3001/coping_skills', {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(copingSkillData)
+        })
+            .then( resp => resp.json() )
+            .then( coping_skill => {
+                dispatch(addCopingSkill(coping_skill))
+            })
     }
 }
