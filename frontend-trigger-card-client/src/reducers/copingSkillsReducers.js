61af2317b0857b541c0ee097c54690dc12d70f44
barrettanderson <barrettanderson1989@gmail.com>
2020-10-13 22:08:03 -0700

    uncommented case ADD_COPING_SKILL

--- frontend-trigger-card-client/src/reducers/copingSkillsReducers.js
+++ frontend-trigger-card-client/src/reducers/copingSkillsReducers.js
@@ -2,8 +2,8 @@ export default (state=[], action) => {
     switch(action.type) {
         case "SHOW_COPING_SKILLS":
             return action.coping_skills
-        // case "ADD_COPING_SKILL":
-        //     return[...state, action.coping_skill]     
+        case "ADD_COPING_SKILL":
+            return[...state, action.coping_skill]     
         default:
             return state;
     }
