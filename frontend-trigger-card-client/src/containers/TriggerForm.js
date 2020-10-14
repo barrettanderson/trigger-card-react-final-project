e607b382da609cf70932fe5ec6b623ee04dfa5c6
barrettanderson <barrettanderson1989@gmail.com>
2020-10-13 17:18:33 -0700

    basic build for trigger form

---
+++ frontend-trigger-card-client/src/containers/TriggerForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class TriggerForm extends Component {
+    render() {
+        return (
+            <div>
+                <p>test</p>
+            </div>
+        )
+    }
+}
+
+export default TriggerForm
