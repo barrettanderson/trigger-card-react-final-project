160fdcc6a14b816f320bb7bae7a386fcb1293757
barrettanderson <barrettanderson1989@gmail.com>
2020-10-13 21:58:10 -0700

    functionality to post new warning sign

--- frontend-trigger-card-client/src/actions/warningSignActions.js
+++ frontend-trigger-card-client/src/actions/warningSignActions.js
@@ -5,10 +5,34 @@ const showWarningSigns = (warning_signs) => {
     }
 }
 
+const addWarningSign = (warning_sign) => {
+    return {
+        type: "ADD_WARNING_SIGN",
+        warning_sign
+    }
+}
+
 export const fetchWarningSigns = () => {
     return (dispatch) => {
         fetch('http://localhost:3001/warning_signs')
             .then( resp => resp.json() )
             .then( warning_signs => dispatch(showWarningSigns(warning_signs)))
+    }
+}
+
+export const createWarningSign = (warningSignData) => {
+    return (dispatch) => {
+        fetch('http://localhost:3001/warning_signs', {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(warningSignData)
+        })
+            .then( resp => resp.json() )
+            .then( warning_sign => {
+                dispatch(addWarningSign(warning_sign))
+            })
     }
 }
