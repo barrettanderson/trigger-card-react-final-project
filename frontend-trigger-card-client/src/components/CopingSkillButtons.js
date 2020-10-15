4fc48aaf75e4f91282abed0a8308a576a63c96e7
barrettanderson <barrettanderson1989@gmail.com>
2020-10-15 10:24:10 -0700

    added delete button code for coping skill

--- frontend-trigger-card-client/src/components/CopingSkillButtons.js
+++ frontend-trigger-card-client/src/components/CopingSkillButtons.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { deleteCopingSkill } from '../actions/copingSkillActions'
 
 export class CopingSkillButtons extends Component {
+    handleClick = () => {
+        this.props.deleteCopingSkill(this.props.coping_skill.id)
+    }
+    
     render() {
         return (
-            <div>
-                
-            </div>
+            <button onClick={this.handleClick}>x</button>
         )
     }
 }
 
-export default CopingSkillButtons
+export default connect(null, { deleteCopingSkill })(CopingSkillButtons)
