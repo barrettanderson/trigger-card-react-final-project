31a7461fb0b232b101040b9be9f1e44aef3790d8
barrettanderson <barrettanderson1989@gmail.com>
2020-10-15 10:22:14 -0700

    added delete coping skill to reducer

--- frontend-trigger-card-client/src/reducers/copingSkillsReducers.js
+++ frontend-trigger-card-client/src/reducers/copingSkillsReducers.js
@@ -3,7 +3,9 @@ export default (state=[], action) => {
         case "SHOW_COPING_SKILLS":
             return action.coping_skills
         case "ADD_COPING_SKILL":
-            return[...state, action.coping_skill]     
+            return[...state, action.coping_skill]
+        case "DELETE_COPING_SKILL":
+            return state.filter(coping_skill => coping_skill.id !== action.id)   
         default:
             return state;
     }
