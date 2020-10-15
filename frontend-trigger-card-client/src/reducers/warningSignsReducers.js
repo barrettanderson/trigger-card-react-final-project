05e908d1d238bd2189012b5e4059ddb0fb9c80f7
barrettanderson <barrettanderson1989@gmail.com>
2020-10-15 10:13:52 -0700

    added delete warning sign to reducer

--- frontend-trigger-card-client/src/reducers/warningSignsReducers.js
+++ frontend-trigger-card-client/src/reducers/warningSignsReducers.js
@@ -3,7 +3,9 @@ export default (state=[], action) => {
         case "SHOW_WARNING_SIGNS":
             return action.warning_signs
         case "ADD_WARNING_SIGN":
-            return[...state, action.warning_sign]     
+            return[...state, action.warning_sign]
+        case "DELETE_WARNING_SIGN":
+            return state.filter(warning_sign => warning_sign.id !==action.id)
         default:
             return state;
     }
