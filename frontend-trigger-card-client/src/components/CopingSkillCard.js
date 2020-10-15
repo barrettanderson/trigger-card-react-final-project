5cafdbb8389c87dabf2bda8c6c26c7af1bd0ae63
barrettanderson <barrettanderson1989@gmail.com>
2020-10-15 10:18:50 -0700

    imported coping skill button to card

--- frontend-trigger-card-client/src/components/CopingSkillCard.js
+++ frontend-trigger-card-client/src/components/CopingSkillCard.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import CopingSkillButtons from './CopingSkillButtons'
 
 const CopingSkillCard = ({ coping_skill }) => {
     return (
-        <li>{coping_skill.name} - {coping_skill.description}</li>
+        <li>{coping_skill.name} - {coping_skill.description}  <CopingSkillButtons coping_skill={coping_skill}/></li>
     )
 }
 
