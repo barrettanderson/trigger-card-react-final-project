9f60e74a66f5e102f320ba743660a59901880112
barrettanderson <barrettanderson1989@gmail.com>
2020-10-15 10:09:16 -0700

    imported default warning sign buttons into card

--- frontend-trigger-card-client/src/components/WarningSignCard.js
+++ frontend-trigger-card-client/src/components/WarningSignCard.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import WarningSignButtons from './WarningSignButtons'
 
 const WarningSignCard = ({ warning_sign }) => {
     return (
-        <li>{warning_sign.name} - {warning_sign.description}</li>
+        <li>{warning_sign.name} - {warning_sign.description} <WarningSignButtons warning_sign={warning_sign} /></li>
     )
 }
 
