98f26812699e5265cf77e95ad3e68cedc962d249
barrettanderson <barrettanderson1989@gmail.com>
2020-10-15 17:45:39 -0700

    added trigger ws and cp to home

--- frontend-trigger-card-client/src/containers/Home.js
+++ frontend-trigger-card-client/src/containers/Home.js
@@ -1,13 +1,49 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import TriggerCard from '../components/TriggerCard';
+import WarningSignCard from '../components/WarningSignCard';
+import CopingSkillCard from '../components/CopingSkillCard';
+import { connect } from 'react-redux'
 
 export class Home extends Component {
     render() {
+        console.log(this.props)
+        const triggers = this.props.triggers.map((trigger, i) => <TriggerCard key={i} trigger={trigger} />)
+        const warning_signs = this.props.warning_signs.map((warning_sign, i) => <WarningSignCard key={i} warning_sign={warning_sign} />)
+        const coping_skills = this.props.coping_skills.map((coping_skill, i) => <CopingSkillCard key={i} coping_skill={coping_skill}/>)
         return (
             <div>
-                Welcome to your trigger card
+                <div>
+                    Welcome to your trigger card
+                </div>
+                <div>
+                    <h2>Triggers</h2>
+                    <ul>
+                        { triggers }
+                    </ul>
+                </div>
+                <div>
+                    <h2>Warning Signs</h2>
+                    <ul>
+                        { warning_signs }
+                    </ul>
+                </div>
+                <div>
+                    <h2>Coping Skills</h2>
+                    <ul>
+                        { coping_skills }
+                    </ul>
+                </div>
             </div>
         )
     }
 }
 
-export default Home
+const mapStateToProps = state => {
+    return {
+        triggers: state.triggersReducer,
+        warning_signs: state.warningSignsReducer,
+        coping_skills: state.copingSkillsReducer
+    }
+}
+
+export default connect(mapStateToProps)(Home)
